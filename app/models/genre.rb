0f52cbf44144d9e7129d73261f0549e13ce3acd9
Tom Banish <tombanish@gmail.com>
2020-06-11 22:38:34 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.artists.each do |n|
-      binding.pry
+      n.name
     end
   end
 end
